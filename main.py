05d676029263691e9b691b1cbd23d44935739f11
Tolga Arslan <eng.tolgaarslan@gmail.com>
2023-05-10 16:25:56 +0300

    Completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+yol = "PNGs"
+yollar = []
+images = []
+
+for i in os.listdir(yol):
+    yollar.append(yol+'/'+i)
+
+for path in yollar:
+     image = Image.open(path)
+     images.append(image)
+
+images[0].save('animasyon.gif', format='GIF', append_images=images[1:], save_all=True, duration=300, loop=0)
